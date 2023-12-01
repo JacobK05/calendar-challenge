4c358878532a66e26e25ab737916ad5fa54e5cbb
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-12-01 11:10:07 -0600

    added the header date

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = dayjs()
+var formatDate = today.format('MMMM, DD YYYY')
+$('#currentDay').text(formatDate);
+
+var calenderBlocks = [
+  { id: '1', hour: '9', Meridiem: 'am' },
+  { id: '2', hour: '10',Meridiem: 'am' },
+  { id: '3', hour: '11',Meridiem: 'am' },
+  { id: '4', hour: '12',Meridiem: 'pm' },
+  { id: '5', hour: '1', Meridiem: 'pm' },
+  { id: '6', hour: '2', Meridiem: 'pm' },
+  { id: '7', hour: '3', Meridiem: 'pm' },
+  { id: '8', hour: '4', Meridiem: 'pm' },
+  { id: '9', hour: '5', Meridiem: 'pm' }
+  ]
+  
+  console.log(calenderBlocks)
+  var blockContainer = $(".container")
+  calenderBlocks.forEach(function(block){
+    var blockEL = $('<div>')
+    .attr("id", block.id)
+    .addClass('col-2 col-md-1 hour text-center py-3')
+    .text(block.hour + '' + block.Meridiem)
+
+
+    var hourContainer = $('<div>')
+    .addClass('block-Container')
+    .css('padding-bottom', '5px')
+    .append(blockEL)
+  
+    blockContainer.append(hourContainer)
+  })
+
+
 
 $(function (event) {
-  event.preventDefault;
   $('.saveBtn').on("click", function(){
+    event.preventDefault();
     console.log('clicked')
-    localStorage.setItem('text', text)
-    
-
     var text = localStorage.getItem("text")
-
-    var calenderBlocks = [
-    { id: '1', hour: '9', Meridiem: 'am',},
-    { id: '2', hour: '10',Meridiem: 'am' },
-    { id: '3', hour: '11',Meridiem: 'am' },
-    { id: '4', hour: '12',Meridiem: 'pm' },
-    { id: '5', hour: '1', Meridiem: 'pm' },
-    { id: '6', hour: '2', Meridiem: 'pm' },
-    { id: '6', hour: '3', Meridiem: 'pm' },
-    { id: '7', hour: '4', Meridiem: 'pm' },
-    { id: '8', hour: '5', Meridiem: 'pm' }
-    ]
-    
-    console.log(calenderBlocks)
-    var blockContainer = $(".container")
-    calenderBlocks.forEach(function(block){
-      var blockEL = $('<div>')
-      .attr("id", block.id)
-      .addClass('col-2 col-md-1 hour text-center py-3')
-      .text(block.hour + '' + block.Meridiem)
-      var hourContainer = $('<div>')
-      .addClass('block-Container')
-      .css('padding-bottom', '5px')
-      .append(blockEL)
-    
-      blockContainer.append(hourContainer)
-    }) 
-
-
-
-
-
-
-
-
-
+    localStorage.setItem('text', text)
+  
   })
   
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -66,4 +64,3 @@ $(function (event) {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
